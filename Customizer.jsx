c6f0f18a5450e4f50e134f3d1b66c37324275156
Avinash123Avinash <89377645+Avinash123Avinash@users.noreply.github.com>
2024-02-18 15:38:43 +0530

    Update Customizer.jsx

---
+++ Customizer.jsx
@@ -0,0 +1,8 @@
+{/* Download button */}
+            <button className='download-btn' onClick={downloadCanvasToImage}>
+              <img
+                src={download}
+                alt='download_image'
+                className='w-3/5 h-3/5 object-contain'
+              />
+            </button>
